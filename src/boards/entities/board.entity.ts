b29e7b9ee31c2a11592b43c73e565e3c11be4299
pooreumu <pooreum83@gmail.com>
2023-01-14 18:43:44 +0900

    🗃️ 게시글 작성자가 회원인지 아닌지에 대한 정보를 갖는 컬럼 추가

--- src/boards/entities/board.entity.ts
+++ src/boards/entities/board.entity.ts
@@ -3,18 +3,42 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Board {
-  @PrimaryGeneratedColumn('increment')
+  @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    name: 'title',
+    type: 'varchar',
+    comment: '게시글 제목',
+  })
   title: string;
 
-  @Column()
+  @Column({
+    name: 'content',
+    type: 'varchar',
+    comment: '게시글 내용',
+  })
   content: string;
 
-  @Column()
+  @Column({
+    name: 'author_id',
+    type: 'varchar',
+    comment: '회원 아이디',
+  })
   authorId: string;
 
-  @Column()
+  @Column({
+    name: 'author_password',
+    type: 'varchar',
+    comment: '회원 비밀번호',
+    nullable: true,
+  })
   authorPassword: string;
+
+  @Column({
+    name: 'membership',
+    type: 'bool',
+    comment: '회원 여부',
+  })
+  membership: boolean;
 }
