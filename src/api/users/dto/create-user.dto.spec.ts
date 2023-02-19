c9cfc1441741f0e3a2ffe8fddbfdec31ebb7310e
pooreumu <pooreum83@gmail.com>
2023-02-19 17:52:19 +0900

    ✅ 회원가입 dto 유닛 테스트 추가

---
+++ src/api/users/dto/create-user.dto.spec.ts
@@ -0,0 +1,23 @@
+// 📦 Package imports
+import * as bcrypt from 'bcrypt';
+
+// 🌏 Project imports
+import { CreateUserDto } from './create-user.dto';
+
+describe('CreateUserDto TEST', () => {
+  describe('CreateUserDto.toEntity test', () => {
+    it('CreateUserDto.toEntity가 password를 hash하나?', async () => {
+      const nickname = 'nickname';
+      const password = 'password';
+
+      const createUserDto = new CreateUserDto();
+      createUserDto.nickname = nickname;
+      createUserDto.password = password;
+
+      const toEntity = await createUserDto.toEntity();
+
+      expect(toEntity.nickname).toBe(nickname);
+      expect(await bcrypt.compare(password, toEntity.password)).toBe(true);
+    });
+  });
+});
