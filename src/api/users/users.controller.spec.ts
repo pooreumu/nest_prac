ea98969fc64c69c76dc5a717fbda950d66abc765
pooreumu <pooreum83@gmail.com>
2023-02-19 17:51:06 +0900

    ✅ 회원가입 유닛테스트 추가

---
+++ src/api/users/users.controller.spec.ts
@@ -0,0 +1,43 @@
+// 🐱 Nestjs imports
+import { Test, TestingModule } from '@nestjs/testing';
+
+// 🌏 Project imports
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+jest.mock('./users.service');
+
+describe('UsersController', () => {
+  let controller: UsersController;
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [UsersService],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('controller.signUp test 회원가입 테스트', () => {
+    it('controller.signUp을 실행하면 usersService.signUp이 실행되나?', async () => {
+      const nickname = 'nickname';
+      const password = 'password';
+
+      const createUserDto = new CreateUserDto();
+      createUserDto.nickname = nickname;
+      createUserDto.password = password;
+
+      await controller.signUp(createUserDto);
+      expect(service.signUp).toBeCalledTimes(1);
+      expect(service.signUp).toBeCalledWith(createUserDto);
+    });
+  });
+});
